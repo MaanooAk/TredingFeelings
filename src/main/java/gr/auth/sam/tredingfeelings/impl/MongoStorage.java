53fd2e174023dd90abe9820dd94048f1d56d1f74
MaanooAk <akritasak@gmail.com>
2017-11-16 15:52:09 +0200

    MongoStorage: Create placeholder

---
+++ src/main/java/gr/auth/sam/tredingfeelings/impl/MongoStorage.java
@@ -0,0 +1,36 @@
+
+package gr.auth.sam.tredingfeelings.impl;
+
+import org.json.JSONObject;
+
+import gr.auth.sam.tredingfeelings.IStorage;
+
+
+/*
+ * TODO doc
+ */
+public class MongoStorage implements IStorage {
+
+    // TODO Implement
+
+    @Override
+    public void open() {
+        // TODO Implement
+    }
+
+    @Override
+    public void close() {
+        // TODO Implement
+    }
+
+    @Override
+    public void createCollection(String name) {
+        // TODO Implement
+    }
+
+    @Override
+    public void insert(String collection, JSONObject object) {
+        // TODO Implement
+    }
+
+}
