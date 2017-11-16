4f31570b03923ba9af29eccc73c44d39befac415
MaanooAk <akritasak@gmail.com>
2017-11-16 15:48:29 +0200

    TwitterConfig: Created

---
+++ src/main/java/gr/auth/sam/tredingfeelings/impl/TwitterConfig.java
@@ -0,0 +1,70 @@
+
+package gr.auth.sam.tredingfeelings.impl;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Properties;
+
+
+/*
+ * TODO doc
+ */
+public class TwitterConfig {
+
+    private static final String DEFAULT_FILENAME = "twitter.config";
+    private static final String KEY_CONSUMERKEY = "consumerKey";
+    private static final String KEY_CONSUMERSECRET = "consumerSecret";
+
+    public static TwitterConfig fromFile() throws IOException {
+
+        return fromFile(Paths.get(DEFAULT_FILENAME));
+    }
+
+    public static TwitterConfig fromFile(Path path) throws IOException {
+
+        Properties p = new Properties();
+
+        try {
+            p.load(Files.newInputStream(path));
+        } catch (NoSuchFileException e) {
+            Files.createFile(path);
+        }
+
+        if (!p.containsKey(KEY_CONSUMERKEY) || p.getProperty(KEY_CONSUMERKEY).isEmpty()) {
+            throw new IOException("Missing " + KEY_CONSUMERKEY + " in " + path.toAbsolutePath());
+        }
+        if (!p.containsKey(KEY_CONSUMERSECRET) || p.getProperty(KEY_CONSUMERSECRET).isEmpty()) {
+            throw new IOException("Missing " + KEY_CONSUMERSECRET + " in " + path.toAbsolutePath());
+        }
+
+        return new TwitterConfig(p.getProperty(KEY_CONSUMERKEY), p.getProperty(KEY_CONSUMERSECRET));
+    }
+
+    @Deprecated
+    public static TwitterConfig fromStrings(String consumerKey, String consumerSecret) {
+
+        return new TwitterConfig(consumerKey, consumerSecret);
+    }
+
+    //
+
+    private final String consumerKey;
+    private final String consumerSecret;
+
+    public TwitterConfig(String consumerKey, String consumerSecret) {
+        this.consumerKey = consumerKey;
+        this.consumerSecret = consumerSecret;
+    }
+
+    public String getConsumerKey() {
+        return consumerKey;
+    }
+
+    public String getConsumerSecret() {
+        return consumerSecret;
+    }
+
+}
