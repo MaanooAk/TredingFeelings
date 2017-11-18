ea1feed394b5d9d8b2f9ab24f3744f09c018816a
Theodoros Bozinis <teodor@bozinis.net>
2017-11-18 22:09:56 +0200

    Mongo Storage: added drop functionality for debug reasons

--- src/main/java/gr/auth/sam/tredingfeelings/impl/MongoStorage.java
+++ src/main/java/gr/auth/sam/tredingfeelings/impl/MongoStorage.java
@@ -1,6 +1,10 @@
 
 package gr.auth.sam.tredingfeelings.impl;
 
+import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
 import org.json.JSONObject;
 
 import gr.auth.sam.tredingfeelings.IStorage;
@@ -10,27 +14,47 @@ import gr.auth.sam.tredingfeelings.IStorage;
  * TODO doc
  */
 public class MongoStorage implements IStorage {
+    private MongoDatabase database;
+    private MongoClient mongoClient;
+    private MongoCredential credential;
 
-    // TODO Implement
+    public MongoStorage() {
+    }
 
     @Override
     public void open() {
-        // TODO Implement
+        try {
+            mongoClient = new MongoClient("localhost", 27017);
+
+            credential = MongoCredential.createCredential("admin", "twitter",
+                    "twitter_pass".toCharArray());
+
+            database = mongoClient.getDatabase("twitter");
+
+            System.out.println("Connection established...");
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
     }
 
     @Override
     public void close() {
-        // TODO Implement
+        mongoClient.close();
     }
 
     @Override
     public void createCollection(String name) {
-        // TODO Implement
+        database.createCollection(name);
+        System.out.println("MongoStorage: Collection " + name + " created...");
     }
 
     @Override
     public void insert(String collection, JSONObject object) {
-        // TODO Implement
+        database.getCollection(collection).insertOne(Document.parse(object.toString()));
     }
 
+    @Override
+    public void drop() {
+        mongoClient.dropDatabase("twitter");
+    }
 }
