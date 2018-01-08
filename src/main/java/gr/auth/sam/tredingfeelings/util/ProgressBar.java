85cdabed05d8619ebf6efedf8a710aee48c71dff
MaanooAk <akritasak@gmail.com>
2018-01-08 15:59:07 +0200

    ProgressBar: Basic structure

--- src/main/java/gr/auth/sam/tredingfeelings/util/ProgressBar.java
+++ src/main/java/gr/auth/sam/tredingfeelings/util/ProgressBar.java
@@ -1,6 +1,19 @@
+
 package gr.auth.sam.tredingfeelings.util;
 
-
 public class ProgressBar {
+
+    private String title;
+    private int max;
+
+    public ProgressBar(String title, int max) {
+        this.title = title;
+        this.max = max;
+
+    }
+
+    public void show(int current) {
+
+    }
 
 }
