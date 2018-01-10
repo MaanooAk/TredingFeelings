8a3a87304c565daac27b5910a84ef29305224e47
Theodoros  Bozinis <mpozinit@csd.auth.gr>
2018-01-11 00:47:00 +0200

    Console Progress Bar: impl of show

--- src/main/java/gr/auth/sam/tredingfeelings/util/ConsoleProgressBar.java
+++ src/main/java/gr/auth/sam/tredingfeelings/util/ConsoleProgressBar.java
@@ -1,6 +1,14 @@
 
 package gr.auth.sam.tredingfeelings.util;
 
+import java.util.Locale;
+
+/**
+ * This is a classical unix progress bar. The length of the bar will be 20 chars
+ * between the [, ]
+ * <p>
+ * This object inherits the needed properties from the parent object.
+ */
 public class ConsoleProgressBar extends ProgressBar {
 
     public ConsoleProgressBar(ProgressBar parent, String title, int max) {
@@ -10,12 +18,35 @@ public class ConsoleProgressBar extends ProgressBar {
     @Override
     public void show() {
 
-        // TODO implement
+        float progress = this.current / (float) this.max;
 
-        System.out.println(current + " / " + max); // placeholder
+        StringBuilder bar = new StringBuilder();
+
+        if (this.message != null) {
+
+            bar.append(this.message);
+            bar.append("\n");
+        }
+
+        bar.append("[");
+        for (int i = 0; i < Math.floor(progress * 20); i++) {
+            bar.append("=");
+        }
+        for (int i = (int) Math.floor(progress * 20); i < 20; i++) {
+            bar.append(" ");
+        }
+        bar.append("]\t");
+
+        String temp = String.format(Locale.ENGLISH, "%.2f", progress * 100);
+        bar.append(temp);
+
+        bar.append("%\r");
+
+        System.out.println(bar.toString());
     }
 
     @Override
-    public void close() {}
+    public void close() {
+    }
 
 }
